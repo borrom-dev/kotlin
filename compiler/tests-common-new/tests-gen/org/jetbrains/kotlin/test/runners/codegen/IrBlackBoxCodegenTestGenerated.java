d3ee68a7d0577f0d5d7bbe72d96727a54dee4c6e
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2023-01-23 11:34:48 +0200

    Update tests after switching to LV 1.9

--- compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
+++ compiler/tests-common-new/tests-gen/org/jetbrains/kotlin/test/runners/codegen/IrBlackBoxCodegenTestGenerated.java
@@ -16870,12 +16870,6 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
         }
 
         @Test
-        @TestMetadata("objectInEnum.kt")
-        public void testObjectInEnum() throws Exception {
-            runTest("compiler/testData/codegen/box/enum/objectInEnum.kt");
-        }
-
-        @Test
         @TestMetadata("ordinal.kt")
         public void testOrdinal() throws Exception {
             runTest("compiler/testData/codegen/box/enum/ordinal.kt");
@@ -20457,12 +20451,6 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
             }
 
             @Test
-            @TestMetadata("kt48445.kt")
-            public void testKt48445() throws Exception {
-                runTest("compiler/testData/codegen/box/inference/builderInference/kt48445.kt");
-            }
-
-            @Test
             @TestMetadata("kt48633.kt")
             public void testKt48633() throws Exception {
                 runTest("compiler/testData/codegen/box/inference/builderInference/kt48633.kt");
@@ -20472,12 +20460,6 @@ public class IrBlackBoxCodegenTestGenerated extends AbstractIrBlackBoxCodegenTes
             @TestMetadata("kt49887.kt")
             public void testKt49887() throws Exception {
                 runTest("compiler/testData/codegen/box/inference/builderInference/kt49887.kt");
-            }
-
-            @Test
-            @TestMetadata("kt50520.kt")
-            public void testKt50520() throws Exception {
-                runTest("compiler/testData/codegen/box/inference/builderInference/kt50520.kt");
             }
 
             @Test
