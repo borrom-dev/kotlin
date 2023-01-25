a3d851051f74b2b97dd7ceeb4650c49ff35697ae
Vladimir Sukharev <vladimir.sukharev@jetbrains.com>
2023-01-25 11:52:55 +0100

    Change comment for future test for KT-56177

--- native/native.tests/testData/klibContents/kt56018_value_parameters_annotations.kt
+++ native/native.tests/testData/klibContents/kt56018_value_parameters_annotations.kt
@@ -2,5 +2,5 @@ annotation class Annotation
 
 fun foo(@Annotation arg: Int) {}
 
-// KT-56018 TODO uncomment the line below to see failure on K2/N
+// KT-56177 TODO uncomment the line after KT-56177 is fixed
 //data class Clazz(@Annotation val param: Int)
